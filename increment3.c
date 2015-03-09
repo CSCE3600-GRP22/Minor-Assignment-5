f390272bc9365735b24e504417d9446387d434b2
Andrew Turner <drewt1995@gmail.com>
2015-03-09 18:27:11 -0500

    Fixed the increment file to support threading

--- increment3.c
+++ increment3.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
+#include <pthread.h>
 
-void inc_n(int *n)
+void* inc_n(void* n)
 {
    /* increment n to 100 */
-   while(++(*n) < 1000000000);
+   int *n_pointer = (int *)n;
+   while(++(*n_pointer) < 1000000000);
+   return NULL;
 }
 
 int main()
 {
 
     int x = 0, y = 0;
+    pthread_t xIncrement, yIncrement;
     printf("x: %d, y: %d\n", x, y);
-   
-    inc_n(&x);
+
+
+    pthread_create(&xIncrement, NULL, inc_n, (void *) &x);
     printf("x increment finished\n");
-   
-    inc_n(&y);
+
+    pthread_create(&yIncrement, NULL, inc_n, (void *) &y);
     printf("y increment finished\n");
+
+    pthread_join(xIncrement, NULL);
+    pthread_join(yIncrement, NULL);
+
     printf("x: %d, y: %d\n", x, y);
     return 1;
 }
